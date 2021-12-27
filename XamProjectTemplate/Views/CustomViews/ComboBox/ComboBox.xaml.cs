21cfd03e5057fccca225ba583865335c3e0f2ca9
Christian Jay Tautu-an <chray221@gmail.com>
2021-12-27 11:24:33 +0800

    deleted unused files

---
+++ XamProjectTemplate/Views/CustomViews/ComboBox/ComboBox.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace XamProjectTemplate
+{
+    public partial class ComboBox
+    {
+        public ComboBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ XamProjectTemplate/Views/CustomViews/ComboBox/ComboBoxListPopup.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamProjectTemplate.Views.CustomViews.ComboBox
+{
+    public partial class ComboBoxListPopup : ContentPage
+    {
+        public ComboBoxListPopup()
+        {
+            InitializeComponent();
+        }
+    }
+}
